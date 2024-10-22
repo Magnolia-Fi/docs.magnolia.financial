b2a7872b33152d4dc3272cabf689e7c2756c11bd
Zach Bloomquist <git@chary.us>
2024-10-21 20:31:53 -0400

    Begin adding search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -201,6 +201,13 @@ const config: Config = {
         } satisfies Plugin.PluginOptions,
       },
     ],
+    [
+      require.resolve("@cmfcmf/docusaurus-search-local"),
+      {
+        indexDocs: true,
+        language: 'en'
+      },
+    ],
   ],
 
   themes: ["docusaurus-theme-openapi-docs"],
